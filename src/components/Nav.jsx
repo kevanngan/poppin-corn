284516a67e1bcefdf02c304cf22cabeb63d33556
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-28 22:31:55 -0700

    Add navigation links

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import {
+    tabletWidth
+} from '../globals/globalVariables';
+
+const Nav = ({ toggleNav, scrollToTop }) => {
+
+    const closeNav = (e) => {
+        if (window.innerWidth < tabletWidth) {
+            toggleNav();
+        } else {
+            e.target.blur();
+        }
+    }
+
+    return (
+        <nav className='siteNavigation' onClick={closeNav}>
+            <ul>
+                <li><NavLink to="/" onClick={scrollToTop}>Home</NavLink></li>
+                <li><NavLink to="/my-list" onClick={scrollToTop}>My List</NavLink></li>
+                <li><NavLink to="/about" onClick={scrollToTop}>About</NavLink></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
