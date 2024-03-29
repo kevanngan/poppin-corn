0725613ea9a8017baf4052cb6f11698faf678bea
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-29 16:58:46 -0700

    Add and update content for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
-// import { scrollToTop } from '../globals/utilityFunctions';
+import { useState, useEffect } from 'react';
+import { scrollToTop } from '../globals/utilityFunctions';
 
+import { tabletWidth } from '../globals/globalVariables';
+
+import HamburgerMenu from './HamburgerMenu';
 import Nav from './Nav';
 
 const Header = () => {
 
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  const isDesktop = (e) => {
+    if (e.matches) {
+      setShowNav(false);
+    }
+  }
+
+  useEffect(() => {
+    let mediaQuery = window.matchMedia(`(min-width: ${tabletWidth}px)`);
+    mediaQuery.addEventListener('change', isDesktop);
+
+    return () => mediaQuery.removeEventListener('change', isDesktop);
+  }, []);
+
   return (
-    <header>
-      <Nav />
+    <header className={showNav ? 'show' : ''}>
+      <HamburgerMenu showNav={showNav} toggleNav={toggleNav} />
+      <Nav toggleNav={toggleNav} scrollToTop={scrollToTop} />
     </header>
   )
 }
