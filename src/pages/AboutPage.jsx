49a313a3c17411c2c90cb8e0e48314bb71cd57d5
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-06 05:27:51 -0700

    Added headers for each paragraph

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -17,9 +17,12 @@ const AboutPage = () => {
           <h2>ABOUT</h2>
           <h3>Welcome to Poppin'Corn! - where movies pop to life!</h3>
           <p>Hey there, fellow movie buff! Grab your bucket of buttery popcorn and get ready to dive into the ultimate movie experience. Here at Poppin' Corn, we're all about celebrating the magic of cinema while munching on the most delightful snack - popcorn!</p>
+          <h3>Introducing the magic of cinema and popcorn!</h3>
           <p>Picture this: you're snuggled up on your couch, popcorn in hand, scrolling through endless movie options. That's where Poppin' Corn comes in! We're your go-to destination for discovering the latest blockbusters, hidden gems, and everything in between.</p>
           <img src={aboutImage} alt="Person sitting eating popcorn" />
+          <h3>Your destination for movie discovery!</h3>
           <p>But wait, there's more! Not only can you explore a vast collection of flicks, but you can also curate your very own movie list. Yep, that's right - bookmark your favorites, create watchlists, and let the popcorn-fueled movie marathons begin!</p>
+          <h3>It's time to pop some corn and kick back!</h3>
           <p>So, whether you're craving a heartwarming rom-com, an adrenaline-pumping action flick, or a spine-tingling thriller, Poppin' Corn has got you covered. It's time to pop some corn, kick back, and let the movie magic unfold. Welcome to the tastiest movie experience on the web!</p>
         </div>
 
