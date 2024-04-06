185006e043479bc9db0f1c9bff120e6ccb56f0c7
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-05 21:59:05 -0700

    Add about image

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { appTitle } from '../globals/globalVariables';
 
 import tmdbAttributionLogo from '../assets/images/tmdb-attribute-logo.svg';
+import aboutImage from '../assets/images/about-image.jpg';
 
 const AboutPage = () => {
 
@@ -17,6 +18,7 @@ const AboutPage = () => {
           <h3>Welcome to Poppin'Corn! - where movies pop to life!</h3>
           <p>Hey there, fellow movie buff! Grab your bucket of buttery popcorn and get ready to dive into the ultimate movie experience. Here at Poppin' Corn, we're all about celebrating the magic of cinema while munching on the most delightful snack - popcorn!</p>
           <p>Picture this: you're snuggled up on your couch, popcorn in hand, scrolling through endless movie options. That's where Poppin' Corn comes in! We're your go-to destination for discovering the latest blockbusters, hidden gems, and everything in between.</p>
+          <img src={aboutImage} alt="Person sitting eating popcorn" />
           <p>But wait, there's more! Not only can you explore a vast collection of flicks, but you can also curate your very own movie list. Yep, that's right - bookmark your favorites, create watchlists, and let the popcorn-fueled movie marathons begin!</p>
           <p>So, whether you're craving a heartwarming rom-com, an adrenaline-pumping action flick, or a spine-tingling thriller, Poppin' Corn has got you covered. It's time to pop some corn, kick back, and let the movie magic unfold. Welcome to the tastiest movie experience on the web!</p>
         </div>
