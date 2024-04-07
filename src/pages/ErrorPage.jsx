322a3ec9d8bef94ac8de98c001b3c753d94967d0
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-07 04:17:03 -0700

    Add content for error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return (
-    <div>
-      <h1>Error Page</h1>
-      <p>This is the error page content.</p>
-    </div>
-  );
-};
+
+    useEffect(() => {
+        document.title = `404 Page Not Found`;
+    }, []);
+
+    return (
+        <main className="errorPage">
+            <h2>404 Error</h2>
+            <p>It looks like there's been an error while trying to process your request.</p>
+            <p>Don't worry, our team of movie-loving techies is on it!</p>
+            <p>In the meantime, you can always go back to <Link to="/">Back to Home</Link> and continue your movie adventure.</p>
+
+            <Link to="/">Back to Home</Link>
+        </main>
+    )
+}
 
 export default ErrorPage;
