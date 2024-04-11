7df23e723d266ea88f980e2264c91ad67c94cde3
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-10 20:54:28 -0700

    Innit MovieTabs

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,5 +1,43 @@
-function MovieTabs() {
+import { useEffect, useState, useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { API_KEY, POPULAR_ENDPOINT, NOW_PLAYING_ENDPOINT, TOP_RATED_ENDPOINT, UPCOMING_ENDPOINT } from '../globals/globalVariables';
+import { isInMyList, createMovieObject } from '../globals/utilityFunctions';
+import MovieCard from './MovieCard';
 
+function MovieTabs({ myList }) {
+    const [currentTab, setCurrentTab] = useState('popular'); // State variable to track the currently active tab
+    const [movies, setMovies] = useState([]); // State variable to store the list of movies fetched from the API
+    const tabsRefs = useRef({}); // Reference object to store references to tab elements
+    const [displayCount, setDisplayCount] = useState(12); // State variable to control the number of movies displayed on the page
+    const [currentPage, setCurrentPage] = useState(1); // State variable to keep track of the current page number for pagination
+    const { tab } = useParams(); // Get the tab parameter from the URL using useParams hook
+    const tabContentRef = useRef(null); // Reference object to store reference to the tab content element for scrolling
+    const navigate = useNavigate(); // useNavigate hook to navigate programmatically to different URLs
+
+    const tabs = {
+        popular: POPULAR_ENDPOINT,
+        now_playing: NOW_PLAYING_ENDPOINT,
+        top_rated: TOP_RATED_ENDPOINT,
+        upcoming: UPCOMING_ENDPOINT,
+    };
+
+    // useEffect to set tab based on url, and scroll to tab from external page
+    useEffect(() => {
+        if (tab && tabs[tab]) {
+            setCurrentTab(tab);
+        } else {
+            navigate('/'); // Redirect to a valid tab if URL param is invalid
+            return;
+        }
+        // Delayed scroll to account for dynamic content loading
+        setTimeout(() => {
+            if (tabContentRef.current) {
+                const position = tabContentRef.current.getBoundingClientRect().top + window.pageYOffset;
+                const offset = 90; // Adjust as needed
+                window.scrollTo({ top: position - offset, behavior: 'smooth' });
+            }
+        }, 100); // Adjust delay as necessary
+    }, [tab, currentTab, navigate]);
 }
 
 export default MovieTabs;
