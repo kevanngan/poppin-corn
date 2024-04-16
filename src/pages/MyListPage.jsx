c534047db4dc774211165ccb31fdc71ee1760e98
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-15 23:49:07 -0700

    Create my list page

--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
+import { appTitle } from "../globals/globalVariables";
+import MovieCard from "../components/MovieCard";
 
 const MyListPage = () => {
-  return (
-    <div>
-      <h1>MyList Page</h1>
-      <p>This is the MyList page content.</p>
-    </div>
-  );
-};
+    // Get myList movies from state
+    const myList = useSelector((state) => state.myList.items);
+    
+    // Set local state with the list from state
+    const [myLocalList, setMyLocalList] = useState(myList);
+
+    // Update local state when state changes
+    useEffect(() => {
+        setMyLocalList(myList); // Update local state with myList from state
+    }, [myList]); // Listen for changes in myList from state
+
+    useEffect(() => {
+        document.title = `${appTitle} - My List`;    
+    }, []);
+
+    return (
+        <main>
+            {/* <section className="" */}
+            <div className="my-list">
+                <h1>MY LIST</h1>
+                {myLocalList.length === 0 ? (
+                    <p className="empty-list" >Looks like your popcorn bag is empty! 🍿 Add some flavor to your movie list by marking your favorites and make your Poppin'Corn experience even more enjoyable! 🎬</p>
+                ) : (
+                    <div className="movie-list">
+                        {myLocalList.map(movie => (
+                            <MovieCard key={movie.id} movie={movie} isInMyList={true}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </main>
+    )
+}
 
 export default MyListPage;
